21f8e24c14e344ca1e2d07ed9657df9c57f0cd3d
pewox <117673001+pewox@users.noreply.github.com>
2023-12-06 16:30:05 +0100

    Update w32_64_software.ps1

--- w32_64_software.ps1
+++ w32_64_software.ps1
@@ -1,2 +1,26 @@
-Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion | Where-Object { ($_.DisplayName -notlike "*Update*") -and ($_.DisplayName -ne $NULL) } | sort DisplayName
-Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion | Where-Object { ($_.DisplayName -notlike "*Update*") -and ($_.DisplayName -ne $NULL) } | sort DisplayName
+$pfad1 = Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion | Where-Object { ($_.DisplayName -notlike "*Update*") -and ($_.DisplayName -ne $NULL) } | sort DisplayName
+$pfad2 = Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion | Where-Object { ($_.DisplayName -notlike "*Update*") -and ($_.DisplayName -ne $NULL) } | sort DisplayName
+$pfad = $pfad1 + $pfad2
+$replace = @{'Microsoft'='MS'
+        'Visual'='Vis'
+        'McAfee Endpoint Security'='McAfee ENS' 
+        'Redistributable'='Red'
+        'Additional'='Ad'
+        'Minimum'='Min'
+        'Windows'='Win'
+        'AnyConnect Secure Mobility Client'='ACSM Client' 
+        'Language Pack'='lang'}
+
+$pfad_s = $pfad | sort -Unique DisplayName
+$obj = foreach($val in $pfad_s){
+    $name = $val.DisplayName
+    $version = $val.DisplayVersion
+        foreach($key in $replace.Keys){
+            $name = $name.replace($key, $replace[$key])
+        }
+            [PSCustomObject]@{
+            Name = $name
+            Version = $version
+            }    
+    }   
+$obj | Format-Table
