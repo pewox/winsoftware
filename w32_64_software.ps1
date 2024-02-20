6019e0d42553e19763b987a7c2516495a38ec2ac
pewox <117673001+pewox@users.noreply.github.com>
2024-02-20 10:53:10 +0100

    Update w32_64_software.ps1

--- w32_64_software.ps1
+++ w32_64_software.ps1
@@ -8,19 +8,26 @@ $replace = @{'Microsoft'='MS'
         'Additional'='Ad'
         'Minimum'='Min'
         'Windows'='Win'
-        'AnyConnect Secure Mobility Client'='ACSM Client' 
+        'Strumenti di correzione di '=''
+        'Outils de vérification linguistique de '=''
+        'lang - DEU'='de'
+        'German'='DE'
+        'High Definition'='HD'
+        'Software Development Kit' = 'SDK'
+        'AnyConnect Secure Mobility Client'='ACSM Client'
         'Language Pack'='lang'}
-        
 $pfad_s = $pfad | sort -Unique DisplayName
-$obj = foreach($val in $pfad_s){
+$sw_liste = ''
+foreach($val in $pfad_s){
     $name = $val.DisplayName
     $version = $val.DisplayVersion
         foreach($key in $replace.Keys){
             $name = $name.replace($key, $replace[$key])
         }
-            [PSCustomObject]@{
-            Name = $name -replace '\-?\s?[v]?\s?(\d{1,}\.)+\d{1,}([-+]\d{1,})?', ''
-            Version = $version
-            }    
-    }   
-$obj | Format-Table
+        $name = $name -replace '\-?\s?[v]?\s?(\d{1,}\.)+\d{1,}([-+]\d{1,})?', '' 
+        $name_aus = '{0}({1}),' -f $name, $version
+        $sw_liste += $name_aus
+        #Write-Host ('0 "WinSoftware" - {0} version {1}' -f $name, $version)
+}
+$sw_liste_aus = '0 "WinSoftware" - {0}' -f $sw_liste
+Write-Host $sw_liste_aus
